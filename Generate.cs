8e5f7c82b6a2b8ab151aca6d50f7a6683e2f13ce
Тимур <tupaev05@mail.ru>
2023-04-22 21:11:59 +0300

    Добавьте файлы проекта.

---
+++ Generate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauss
+{
+    public class Generate
+    {
+        public static double[][] GenerateRandomMatrix(int size)
+        {
+            double[][] matrix = new double[size][];
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i] = new double[size];
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i][j] = rand.NextDouble() * 2 - 1;
+                }
+            }
+            return matrix;
+        }
+
+        public static double[] GenerateRandomVector(int size)
+        {
+            double[] vector = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                vector[i] = rand.NextDouble() * 2 - 1;
+            }
+            return vector;
+        }
+
+        static Random rand = new Random();
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace Gauss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSizes = { 100, 1000,10000  };
+            int[] threadCounts = { 1, 2, 4, 8, 16 };
+            
+            foreach (int size in matrixSizes)
+            {
+                
+                Console.WriteLine("size: " + size);
+                double[][] matrix = Generate.GenerateRandomMatrix(size);
+                double[] vector = Generate.GenerateRandomVector(size);
+
+                foreach (int threadCount in threadCounts)
+                {
+                    Console.WriteLine("Thread count: " + threadCount);
+
+                    
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    GaussThread.SolveGaussThread(matrix, vector, threadCount);
+                    stopwatch.Stop();
+                    Console.WriteLine("Thread  time: " + stopwatch.Elapsed.TotalMilliseconds );
+
+                    
+                    stopwatch = Stopwatch.StartNew();
+                    GaussTask.SolveGaussTask(matrix, vector, threadCount);
+                    stopwatch.Stop();
+                    Console.WriteLine("Task  time: " + stopwatch.Elapsed.TotalMilliseconds );
+
+                    
+                    stopwatch = Stopwatch.StartNew();
+                    GaussParallelFor.SolveGaussParallelFor(matrix, vector, threadCount);
+                    stopwatch.Stop();
+                    Console.WriteLine("Parallel.For  time: " + stopwatch.Elapsed.TotalMilliseconds);
+                }
+            }
+        }
+    }
+}
